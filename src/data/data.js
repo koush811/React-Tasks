68d833925b9a01aea886e6c1ab7a6ce6cb9e7c1f
kosei <123241044+koush811@users.noreply.github.com>
2026-06-28 20:36:37 +0900

    card表示

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,20 @@
 const cardContent = [
     {
         id: 1,
-        
+        name: "カード１",
+        text: "テキストーーーー",
+        img: "src/imgs/1-392a39b8.jpg"
     },
     {
-
+        id: 1,
+        name: "カード１",
+        text: "テキストーーーー",
+        img: "src/imgs/9a5165d9180661b9012578ee91783576.jpeg"
     },
     {
-
+        id: 1,
+        name: "カード１",
+        text: "テキストーーーー",
+        img: "src/imgs/1-392a39b8.jpg"
     }
 ]
