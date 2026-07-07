46384beca8350a0cb2d32fcda2992d89a6554b68
kosei <123241044+koush811@users.noreply.github.com>
2026-07-07 22:33:53 +0900

    sort機能追加

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,8 +2,19 @@ import img from "../imgs/404.png"
 import "./components.css"
 import Card from "./card"
 import { cardContent } from "../data/data"
+import { useState } from "react"
 
 function About(){
+
+    const [sortType,setSortType] = useState("normal")
+
+    let cards = ""
+    if(sortType === "sort"){
+        cards = [...cardContent].sort((a,b) => b.star - a.star) 
+    }else{
+        cards = [...cardContent]
+    }
+
     return(
         <>
             <div className="title-content">
@@ -12,7 +23,11 @@ function About(){
             </div>
         
             <div className="card-area">
-                {cardContent.map(card => (
+                <select value={sortType} onChange={(e) => setSortType(e.target.value)}>
+                    <option value="normal">通常</option>
+                    <option value="sort">評価順</option>
+                </select>
+                {cards.map(card => (
                     <Card key={card.id} name={card.name} text={card.text} img={card.img} star={card.star}></Card>
                 ))}
             </div>
